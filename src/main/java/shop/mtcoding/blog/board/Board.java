e36ddec1a4a81da9aa998522fffe8e76b340f8a7
kimseongjae <stwin755@gmail.com>
2024-02-08 10:33:39 +0900

    목록보기 완성

---
+++ src/main/java/shop/mtcoding/blog/board/Board.java
@@ -0,0 +1,15 @@
+package shop.mtcoding.blog.board;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "board_tb")
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String title;
+    private String content;
+}
---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,7 @@
+insert into board_tb(title, content) values ('제목1', '내용1');
+insert into board_tb(title, content) values ('제목2', '내용2');
+insert into board_tb(title, content) values ('제목3', '내용3');
+insert into board_tb(title, content) values ('제목4', '내용4');
+insert into board_tb(title, content) values ('제목5', '내용5');
+insert into board_tb(title, content) values ('제목6', '내용6');
+insert into board_tb(title, content) values ('제목7', '내용7');
